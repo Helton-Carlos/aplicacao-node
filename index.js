c3634e9234dedea4739c8ae5919ba560fc7f4ef5
Helton Carlos <heltoncarlospa@hotmail.com>
2021-10-05 22:16:16 -0300

    texto pego por parametros

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const texto = "Helton Brito";
+/*const texto = "Helton Brito";
 function nome(string){
     return string
 }
+console.log(nome(texto))*/
 
-console.log(nome(texto))
+const chalk = require('chalk')
+const fs = require('fs')
+
+function pegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8'
+    fs.readFile(caminhoDoArquivo, encoding, (_,data)=>{
+        console.log(chalk.green(data))
+    })
+}
+
+pegaArquivo('./arquivo/texto1.md')
