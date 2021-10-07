819ecc0a6e50ed760ac588d9d31e13fa80627eda
Helton Carlos <heltoncarlospa@hotmail.com>
2021-10-06 23:04:20 -0300

    async

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 function nome(string){
     return string
 }
-console.log(nome(texto))*/
+console.log(nome(texto))
 
 const chalk = require('chalk')
 const fs = require('fs')
-
 function trataErro(erro){
     throw new Error(chalk.red(erro.code,'erro provisório'));
 }
@@ -18,16 +17,14 @@ function pegarArquivo(caminhoDoArquivo){
     .then((texto)=>chalk.green(console.log(texto)))
     .catch((erro)=>trataErro)
 }
-
 pegaArquivo('./arquivo/texto.md')
-/*
+
+
 const chalk = require('chalk')
 const fs = require('fs')
-
 function trataErro(erro){
     throw new Error(chalk.red(erro.code,'erro provisorio'));
 }
-
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8'
     fs.readFile(caminhoDoArquivo, encoding, (erro,data)=>{
@@ -37,5 +34,17 @@ function pegaArquivo(caminhoDoArquivo){
         console.log(chalk.green(data))
     })
 }
-
 pegaArquivo('./arquivo/texto.md')*/
+
+const chalk = require("chalk");
+const fs = require('fs');
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'))
+}
+async function pegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8';
+  try{
+    const texto = await fs.promises.readFile(caminhoDoArquivo,encoding);
+    console.log(chalk.green(texto))
+  }
+}
